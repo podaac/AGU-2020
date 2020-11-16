5741c4991109bca0a6374d3a5d52ad4f0b71e3c8
Andy Barrett <apbarret@nsidc.org>
2020-11-16 15:15:49 -0700

    add conversion from results to geopandas

--- tutorial_03/tutorial_cmr.py
+++ tutorial_03/tutorial_cmr.py
@@ -2,6 +2,10 @@
 import requests
 import itertools
 
+from shapely.geometry import Polygon
+import pandas as pd
+import geopandas
+
 
 def search_granules(search_parameters, geojson=None, output_format="json"):
     """
@@ -120,3 +124,129 @@ def filter_spatiotemporal(search_results):
         subset.append(_extractor(entry))
     
     return subset
+
+
+def polygon_to_geometry(polygon_entry):
+    L = [float(s) for s in list(itertools.chain.from_iterable(polygon_entry))[0].split()]
+    return Polygon( list(zip(L[1::2], L[::2])) )  # CMR returns JSON with latitude first  
+
+
+def results_to_geodataframe(search_results, crs="EPSG:4326"):
+    """
+    Converts spatiotemporal fields from results to geopandas DataFrame
+    
+    :search_results: returned by search_granules
+    :crs: coordinate reference system: default is WGS84 (EPSG:4326)
+    
+    :returns: geopandas dataframe with polygons field as geometry
+    """
+    
+    results_dict = filter_spatiotemporal(search_results)
+    if not results_dict:
+        return
+    
+    df = pd.results_df = pd.DataFrame(results_dict)
+    df["geometry"] = [polygon_to_geometry(entry["polygons"]) for entry in results_dict]
+    return geopandas.GeoDataFrame(df, crs=crs)
+    
+
+def get_username():
+    username = ''
+
+    # For Python 2/3 compatibility:
+    try:
+        do_input = raw_input  # noqa
+    except NameError:
+        do_input = input
+
+    while not username:
+        try:
+            username = do_input('Earthdata username: ')
+        except KeyboardInterrupt:
+            quit()
+    return username
+
+
+def get_password():
+    password = ''
+    while not password:
+        try:
+            password = getpass('password: ')
+        except KeyboardInterrupt:
+            quit()
+    return password
+
+
+def get_credentials(url):
+    """Get user credentials from .netrc or prompt for input."""
+    credentials = None
+    errprefix = ''
+    try:
+        info = netrc.netrc()
+        username, account, password = info.authenticators(urlparse(URS_URL).hostname)
+        errprefix = 'netrc error: '
+    except Exception as e:
+        if (not ('No such file' in str(e))):
+            print('netrc error: {0}'.format(str(e)))
+        username = None
+        password = None
+
+    while not credentials:
+        if not username:
+            username = get_username()
+            password = get_password()
+        credentials = '{0}:{1}'.format(username, password)
+        credentials = base64.b64encode(credentials.encode('ascii')).decode('ascii')
+
+        if url:
+            try:
+                req = Request(url)
+                req.add_header('Authorization', 'Basic {0}'.format(credentials))
+                opener = build_opener(HTTPCookieProcessor())
+                opener.open(req)
+            except HTTPError:
+                print(errprefix + 'Incorrect username or password')
+                errprefix = ''
+                credentials = None
+                username = None
+                password = None
+
+    return credentials
+
+
+def cmr_download(urls, outpath=''):
+    """Download files from list of urls."""
+    if not urls:
+        return
+
+    url_count = len(urls)
+    print('Downloading {0} files...'.format(url_count))
+    credentials = None
+
+    for index, url in enumerate(urls, start=1):
+        if not credentials and urlparse(url).scheme == 'https':
+            credentials = get_credentials(url)
+
+        filename = os.path.join(outpath, url.split('/')[-1])
+        print('{0}/{1}: {2}'.format(str(index).zfill(len(str(url_count))),
+                                    url_count,
+                                    filename))
+
+        try:
+            # In Python 3 we could eliminate the opener and just do 2 lines:
+            # resp = requests.get(url, auth=(username, password))
+            # open(filename, 'wb').write(resp.content)
+            req = Request(url)
+            if credentials:
+                req.add_header('Authorization', 'Basic {0}'.format(credentials))
+            opener = build_opener(HTTPCookieProcessor())
+            data = opener.open(req).read()
+            open(filename, 'wb').write(data)
+        except HTTPError as e:
+            print('HTTP error {0}, {1}'.format(e.code, e.reason))
+        except URLError as e:
+            print('URL error: {0}'.format(e.reason))
+        except IOError:
+            raise
+        except KeyboardInterrupt:
+            quit()
